6430d0e2b0f15a3c1cc9600048475d96d6e8621f
KubicekPetr <kubicekpetr@outlook.com>
2021-02-15 22:16:32 +0100

    mirroring the angular app with react app since now

--- pw-react/src/App.js
+++ pw-react/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    password: '',
+  }
+
+  onButtonClick = () => {
+    this.setState({password: 'My password'});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Password generator</h1>
+  
+        <div>
+          <label>Enter length</label>
+          <input type="text" />
+        </div>
+  
+        <div>
+          <label>Use letters</label>
+          <input type="checkbox" />
+        </div>
+        <div>
+          <label>Use numbers</label>
+          <input type="checkbox" />
+        </div>
+        <div>
+          <label>Use symbols</label>
+          <input type="checkbox" />
+        </div>
+  
+        <div>
+          <button onClick={this.onButtonClick}>Generate</button>
+        </div>
+  
+        <div>
+          <label>Your password</label>
+          <input defaultValue={this.state.password} type="text" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
