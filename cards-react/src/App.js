73495008897cff7dae25c3d8711e521cb76943db
KubicekPetr <kubicekpetr@outlook.com>
2021-02-19 23:15:31 +0100

    creating html dump and importing static images and styling them in both projects

--- cards-react/src/App.js
+++ cards-react/src/App.js
@@ -2,7 +2,22 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src="tree.jpeg" alt="tree" />
+        </figure>
+      </div>
+
+      <div class="card-content">
+        <div class="media-content">
+          <p class="title is-4">Neat tree</p>
+          <p class="subtitle is-6">@nature</p>
+        </div>
+        <div class="content">
+          Saw this awesome tree during my hike today.
+        </div>
+      </div>
     </div>
   );
 }
