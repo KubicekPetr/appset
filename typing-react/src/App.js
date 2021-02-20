0c7e9f73d97e83f4a22cf6fae3925d39eee5bba3
KubicekPetr <kubicekpetr@outlook.com>
2021-02-20 20:05:58 +0100

    creating html templates with randomly generated sentence

--- typing-react/src/App.js
+++ typing-react/src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { lorem } from 'faker';
+
+const sentence = lorem.sentence();
 
 function App() {
   return (
-    <div className="App">
-    
+    <div className="container">
+      <div className="columns is-centered">
+        <div className="box column is-one-third">
+          <h1 className="title has-text-centered">Typing challenge</h1>
+
+          <p className="has-text-centered">
+            { sentence }
+          </p>
+
+          <div className="field">
+            <input className="input" type="text" />
+          </div>
+
+          <div className="box has-text-centered">
+            <h2 className="title">You win</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
