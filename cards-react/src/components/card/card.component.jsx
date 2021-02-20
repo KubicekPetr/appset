79000dd2e6d3594036b2d9bed58139fb96dd5c92
KubicekPetr <kubicekpetr@outlook.com>
2021-02-20 14:46:04 +0100

    moving our dumps of cards into card components

--- cards-react/src/components/card/card.component.jsx
+++ cards-react/src/components/card/card.component.jsx
@@ -1,8 +1,22 @@
 import './card.component.css';
 
 const Card = () => (
-    <div>
-        <p>card works!</p>
+    <div className="card">
+        <div className="card-image">
+            <figure className="image is-4by3">
+                <img src="tree.jpeg" alt="tree" />
+            </figure>
+        </div>
+
+        <div className="card-content">
+            <div className="media-content">
+                <p className="title is-4">Neat tree</p>
+                <p className="subtitle is-6">@nature</p>
+            </div>
+            <div className="content">
+                Saw this awesome tree during my hike today.
+            </div>
+        </div>
     </div>
 );
 
