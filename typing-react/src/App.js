4acf2d1e55d3ac44f9bf10667217d3b746ee2e03
KubicekPetr <kubicekpetr@outlook.com>
2021-02-21 21:33:06 +0100

    React - coloring each letter agains correctness of inputLetter

--- typing-react/src/App.js
+++ typing-react/src/App.js
@@ -4,14 +4,22 @@ import { lorem } from 'faker';
 
 const sentence = lorem.sentence();
 
-const handleUserInput = (event) => {
-  console.log(event.target.value);
-};
-
 class App extends React.Component {
   state = {
-    correct: false,
+    inputSentence: '',
   }
+
+  handleUserInput = (event) => {
+    this.setState({
+      inputSentence: event.target.value,
+    })
+  };
+
+  colorLetters(letter, inputLetter) {
+    return inputLetter === letter ? {color: '#55ff55'} : 
+    (inputLetter ? {color: '#ff5555'} : {color: 'grey'});
+  }
+
   render() {
     return (
       <div className="container">
@@ -20,15 +28,21 @@ class App extends React.Component {
             <h1 className="title has-text-centered">Typing challenge</h1>
 
             <p className="has-text-centered">
-              {sentence}
+              {
+                [...sentence].map((letter, i) => 
+                <span
+                key={i}
+                style={this.colorLetters(letter, this.state.inputSentence[i])}>
+                {letter}</span>)
+              }
             </p>
 
             <div className="field">
-              <input onInput={handleUserInput} className="input" type="text" />
+              <input onInput={this.handleUserInput} className="input" type="text" />
             </div>
 
             <div 
-            style={{ display: this.state.correct ? 'block' : 'none' }} 
+            style={{ display: this.state.inputSentence === sentence ? 'block' : 'none' }} 
             className="box has-text-centered">
               <h2 className="title">You win</h2>
             </div>
