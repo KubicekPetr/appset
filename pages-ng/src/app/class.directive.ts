10a608e075de084bdf9d60fd7ab24ef161dca6aa
KubicekPetr <kubicekpetr@outlook.com>
2021-03-06 23:54:51 +0100

    passing a property to class directive and slow down the contructor cause directive is initialized before the prop is passed to it

--- pages-ng/src/app/class.directive.ts
+++ pages-ng/src/app/class.directive.ts
@@ -1,12 +1,16 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
+  @Input() backgroundColor: string;
 
-  constructor(private element: ElementRef) { 
-    this.element.nativeElement.style.backgroundColor = 'orange';
+  constructor(private element: ElementRef) {
+    // bad pattern
+    setTimeout(() => {
+      this.element.nativeElement.style.backgroundColor = this.backgroundColor;
+    }, 50);
   }
 
 }
